306729629c918cd59f50b1217b2585f32d519e19
Adrien26-source! <adrien.cher7@gmail.com>
2021-11-28 01:22:19 +0100

    widget 5 nouveaux animaux

--- frontend/src/Test-produits.js
+++ frontend/src/Test-produits.js
@@ -4,37 +4,57 @@ import './Bloc-style.css';
 const Testproduits = props => {
   return(
     <div className="bloc text-center">
-      <h4 className="bloc-title"></h4>
-      <h4 className="bloc-title">Animalerie</h4>
+      <br>
+        </br>
+      <h4 className="bloc-title">Les nouveaux animaux</h4>
+      <br>
+        </br>
       <div className="overflow">
         <img src={props.image1} alt="image1" className="bloc-img-top photos"/>
       </div>
       <div className="bloc-body text-dark">
-        <h4 className="bloc-title">2100€</h4>
-        <p className="bloc-text text-secondary">
+      <h5 className="bloc-title">Nom : Pichoune</h5>
+      <h5 className="bloc-title">Sexe : Male</h5>
+      <h5 className="bloc-title">Age : 2 ans </h5>
+      <h5 className="bloc-title">Type : Chien Golden Retriever</h5>
+        <h4 className="bloc-title">Prix : 1500€</h4>
+        <p className="bloc-text text-primary">
             Chien
         </p>
-        <a href="#" className="btn btn-outline-warning">Voir</a>
+        <a href="#" className="btn btn-outline-danger">Acheter</a>
       </div>
       <div className="overflow">
         <img src={props.image2} alt="image2" className="bloc-img-top photos"/>
       </div>
       <div className="bloc-body text-dark">
-        <h4 className="bloc-title">4000€</h4>
-        <p className="bloc-text text-secondary">
+      <h5 className="bloc-title">Nom : Chouca</h5>
+      <h5 className="bloc-title">Sexe : Femelle</h5>
+      <h5 className="bloc-title">Age : 3 mois </h5>
+      <h5 className="bloc-title">Type : Chat tigré</h5>
+        <h4 className="bloc-title">Prix : 1000€</h4>
+        <p className="bloc-text text-primary">
             Chat
         </p>
-        <a href="#" className="btn btn-outline-warning">Voir</a>
+        <a href="#" className="btn btn-outline-danger">Acheter</a>
       </div>
       <div className="overflow">
         <img src={props.image3} alt="image3" className="bloc-img-top photos"/>
       </div>
       <div className="bloc-body text-dark">
-        <h4 className="bloc-title">8000€</h4>
-        <p className="bloc-text text-secondary">
+      <h5 className="bloc-title">Nom : Baba</h5>
+      <h5 className="bloc-title">Sexe : Male</h5>
+      <h5 className="bloc-title">Age : 5 ans </h5>
+      <h5 className="bloc-title">Type : Panda affamé</h5>
+        <h4 className="bloc-title">Prix : 6000€</h4>
+        <p className="bloc-text text-primary">
             Panda
         </p>
-        <a href="#" className="btn btn-outline-warning">Details</a>
+        <a href="#" className="btn btn-outline-danger">Acheter</a>
+        <br>
+        </br>
+        <br>
+        </br>
+        <a href="#" className="btn btn-outline-warning">Voir plus de résulats</a>
       </div>
     </div>
   );
